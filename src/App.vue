6ef6e35578f6a61794492fc95e4c2abf76bfca04
Contreras <DanielContreras@ppg.com>
2022-05-19 15:53:06 +0200

    items

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <ItemDetails />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ItemDetails from './components/Items.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ItemDetails
   }
 }
 </script>
---
+++ src/components/Items.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <h3> Items:</h3>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">ItemCode</th>
+          <th scope="col">ItemDescription</th>
+          <th scope="col">TechnicalGroup</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in items" v-bind:key="item.ItemCode"> 
+          <th scope="row">{{item.ItemCode}}</th>
+          <td>{{item.ItemDescription}}</td>
+          <td>{{item.TechnicalGroup}}</td>
+        </tr>
+      </tbody>
+    </table> 
+  </div> 
+</template>
+
+<script>
+  import axios from 'axios';
+  export default {
+    name: 'ItemDetails',
+    data() {
+      return {
+        items: null,
+      };
+    },
+    created: function() {
+      axios
+        .get('https://my-json-server.typicode.com/danielcontreras-ppg/myjsonserver-lfaitems/items')
+        .then(res => {
+          this.items = res.data;
+        })
+    }
+  }
+</script>
+
+<style>
+  h3 {
+    margin-bottom: 5%;
+  }
+</style>
