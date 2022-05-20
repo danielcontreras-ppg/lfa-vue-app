4c71da4a62066d1b9a9c14e9d886268bed11cc63
Contreras <DanielContreras@ppg.com>
2022-05-20 10:03:13 +0200

    items-form-disabledide

--- src/components/Items.vue
+++ src/components/Items.vue
@@ -3,7 +3,7 @@
     <h3> Items:</h3>
         <div v-for="item in items" v-bind:key="item.ItemCode">
             <form id="formElem">
-                <input type="text" name="ItemCode" v-model="item.ItemCode">
+                <input type="text" name="ItemCode" v-model="item.ItemCode" disabled />
                 <input type="text" name="ItemDescription" v-model="item.ItemDescription">
                 <input type="text" name="TechnicalGroup" v-model="item.TechnicalGroup">
                 <button type="submit">Submit</button>
