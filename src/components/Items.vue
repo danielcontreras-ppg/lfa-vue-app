b053529283f502a407f0eae13866ac211098e049
Contreras <DanielContreras@ppg.com>
2022-06-07 10:27:27 +0200

    07062022

--- src/components/Items.vue
+++ src/components/Items.vue
@@ -3,10 +3,10 @@
     <h3> Items:</h3>
         <div v-for="item in items" v-bind:key="item.ItemCode">
             <form id="formElem">
-                <input type="text" name="ItemCode" v-model="item.ItemCode" disabled />
-                <input type="text" name="ItemDescription" v-model="item.ItemDescription">
-                <input type="text" name="TechnicalGroup" v-model="item.TechnicalGroup">
-                <button type="submit" v-on:click="submitItem(item.ItemCode, item.ItemDescription, item.TechnicalGroup)">Submit</button>
+                <input type="text" name="itemCode" v-model="item.itemcode" disabled />
+                <input type="text" name="itemDescription" v-model="item.itemdescription">
+                <input type="text" name="technicalGroup" v-model="item.technicalgroup">
+                <button type="submit" v-on:click="submitItem(item.itemcode, item.itemdescription, item.technicalgroup)">Submit</button>
             </form> 
         </div>
   </div> 
@@ -23,17 +23,17 @@
     },
     created: function () {
       axios
-        .get('https://localhost:7188/api/items/getitems')
+        .get('http://items.lfa/api/items/getitems')
         .then(res => {
           this.items = res.data;
         })
     },
     methods: {
         submitItem: function (id, desc, grp) {
-            axios.put('http://localhost/itemsput', {
-                ItemCode: id,
-                ItemDescription: desc,
-                TechnicalGroup: grp
+            axios.put('http://items.lfa/api/items/putitems', {
+                itemcode: id,
+                itemdescription: desc,
+                technicalgroup: grp
             });
         }
   }  
